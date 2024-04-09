d2e7c0fd97bb821a7aa36b4e960ef85ea1a1e95d
Kyxtaka <randriantsoanathan1@gmail.com>
2024-04-09 14:44:57 +0200

    Initialisation avec maven

---
+++ mud/src/test/java/fr/univ_orleans/iut45/mud/JeuxOlympiqueTest.java
@@ -0,0 +1,38 @@
+package fr.univ_orleans.iut45.mud;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class JeuxOlympiqueTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public JeuxOlympiqueTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( JeuxOlympiqueTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
