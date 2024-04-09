21238b81166521e89571d4d840ec6df88c7ef9af
Nathan Randriantsoa <134612654+Kyxtaka@users.noreply.github.com>
2024-04-09 14:49:47 +0200

    ajout de Athlete.java (#1)

---
+++ mud/src/main/java/fr/univ_orleans/iut45/mud/Athlete.java
@@ -0,0 +1,56 @@
+public class Athlete {
+    private String nom;
+    private String prenom;
+    private String sexe;
+    private Pays pays;
+    private Sport sport;
+    
+    public Athlete(String nom, String prenom, String sexe, Pays pays, Sport sport){
+        this.nom = nom;
+        this.prenom = prenom;
+        this.sexe =sexe;
+        this.pays = pays;
+        this.sport = sport;
+    }
+
+    public String getNom() {
+        return this.nom;
+    }
+
+    public String getPrenom() {
+        return this.prenom;
+    }
+
+    public String getSexe() {
+        return this.sexe;
+    }
+
+    public Pays getPays() {
+        return this.pays;
+    }
+
+    public Sport getSport() {
+        return this.sport;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public void setSexe(String sexe) {
+        this.sexe = sexe;
+    }
+
+    public void setPays(Pays pays) {
+        this.pays = pays;
+    }
+
+    public void setSport(Sport sport) {
+        this.sport = sport;
+    }
+
+}
