1fe6f2d8c132470fc20ffffac9d2ee8b2c892514
Nathan Randriantsoa <randriantsoanathan1@gmail.com>
2024-05-30 16:37:00 +0200

    modification de TestSport

--- mud/src/test/java/fr/univ_orleans/iut45/mud/TestSport.java
+++ mud/src/test/java/fr/univ_orleans/iut45/mud/TestSport.java
@@ -1,13 +1,11 @@
 package fr.univ_orleans.iut45.mud;
 
-
-import fr.univ_orleans.iut45.mud.Sport;
-import org.junit.Before;
 import org.junit.*;
 import static org.junit.Assert.assertEquals;
 
 public class TestSport {
-Sport natation = new Sport("Volley");
+    Sport natation = new Sport("Volley");
+    
     @Test
     public void TestName() {
         Sport volley = new Sport("Volley");
