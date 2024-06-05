6fa1f92fc2066ce66c45420bb07b1cbbad9d56db
Nathan Randriantsoa <134612654+Kyxtaka@users.noreply.github.com>
2024-06-05 20:36:36 +0200

    Test athlete (#36)
    * TestSport (Juste getNom) + changement version junit
    
    * modification de la version junit dans pom.xml
    
    * modification de TestSport
    
    * ajout de TestAthlete.java
    
    * mise a jour Test athlete pour que sa fonction avec Junit5 jupiter

--- mud/src/test/java/fr/univ_orleans/iut45/mud/TestAthlete.java
+++ mud/src/test/java/fr/univ_orleans/iut45/mud/TestAthlete.java
@@ -1,41 +1,40 @@
 package fr.univ_orleans.iut45.mud;
 
-import org.junit.*;
-import static org.junit.Assert.assertEquals;
-
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
 public class TestAthlete {
-    private Athlete athlete1;
-    private Athlete athlete2;
-    private Athlete athlete3;
-    private Athlete athlete4;
-    private Athlete athlete5;
-
-    @Before // s'execute avant tous les test
-    public void init() { //initialisation des attribut
-        this.athlete1 = new Athlete("Randriantsoa", "Nathan", "M", null, null, 10, 20,65);
-        this.athlete2 = new Athlete("Voivenel", "Romain", "M", null, null, 11, 21, 66);
-        this.athlete3 = new Athlete("Gangneux", "Pierre", "M", null, null, 12, 22, 67);
-        this.athlete4 = new Athlete("Saunier", "Léo", "M", null, null, 13, 23, 68);
-        this.athlete5 = new Athlete("Random", "Fille", "F", null, null, 14, 24, 69);
-
+    private static Athlete athlete1;
+    private static Athlete athlete2;
+    private static Athlete athlete3;
+    private static Athlete athlete4;
+    private static Athlete athlete5;
+    
+    @BeforeAll // s'execute avant tous les test
+    static void setUp() { //initialisation des attribut
+        athlete1 = new Athlete("Randriantsoa", "Nathan", "M", null, null, 10, 20,65);
+        athlete2 = new Athlete("Voivenel", "Romain", "M", null, null, 11, 21, 66);
+        athlete3 = new Athlete("Gangneux", "Pierre", "M", null, null, 12, 22, 67);
+        athlete4 = new Athlete("Saunier", "Léo", "M", null, null, 13, 23, 68);
+        athlete5 = new Athlete("Random", "Fille", "F", null, null, 14, 24, 69);
     }
 
     @Test
     public void testNomPrenomSex() {
         //athlete 1
-        assertEquals(athlete1.getNom(), "Randriantsoa");
-        assertEquals(athlete1.getPrenom(), "Nathan");
-        assertEquals(athlete1.getSexe(), "M");
+        Assertions.assertEquals(athlete1.getNom(), "Randriantsoa");
+        Assertions.assertEquals(athlete1.getPrenom(), "Nathan");
+        Assertions.assertEquals(athlete1.getSexe(), "M");
 
         //athlete 2
-        assertEquals(athlete2.getNom(), "Voivenel");
-        assertEquals(athlete2.getPrenom(), "Romain");
-        assertEquals(athlete2.getSexe(), "M");
+        Assertions.assertEquals(athlete2.getNom(), "Voivenel");
+        Assertions.assertEquals(athlete2.getPrenom(), "Romain");
+        Assertions.assertEquals(athlete2.getSexe(), "M");
 
         //athlete 5
-        assertEquals(athlete5.getNom(), "Random");
-        assertEquals(athlete5.getPrenom(), "Fille");
-        assertEquals(athlete5.getSexe(), "F");
+        Assertions.assertEquals(athlete5.getNom(), "Random");
+        Assertions.assertEquals(athlete5.getPrenom(), "Fille");
+        Assertions.assertEquals(athlete5.getSexe(), "F");
 
     }
 
@@ -50,8 +49,8 @@ public class TestAthlete {
         athlete4.setPays(allemage);
 
         //test
-        assertEquals(athlete3.getPays(), france);
-        assertEquals(athlete4.getPays().getNom(), "Allemagne");    
+        Assertions.assertEquals(athlete3.getPays(), france);
+        Assertions.assertEquals(athlete4.getPays().getNom(), "Allemagne");    
     }
 
     @Test
@@ -61,14 +60,14 @@ public class TestAthlete {
         Integer forceAthlete5 = 14; Integer agiliteAthlete5 = 24; Integer endurenceAthlete5 = 69;
 
         //test Athlete2
-        assertEquals(this.athlete2.getForce(), forceAthlete2);
-        assertEquals(this.athlete2.getAgilite(), agiliteAthlete2);
-        assertEquals(this.athlete2.getEndurance(), endurenceAthlete2);
+        Assertions.assertEquals(athlete2.getForce(), forceAthlete2);
+        Assertions.assertEquals(athlete2.getAgilite(), agiliteAthlete2);
+        Assertions.assertEquals(athlete2.getEndurance(), endurenceAthlete2);
 
         //test Athlete2
-        assertEquals(this.athlete5.getForce(), forceAthlete5);
-        assertEquals(this.athlete5.getAgilite(), agiliteAthlete5);
-        assertEquals(this.athlete5.getEndurance(), endurenceAthlete5);
+        Assertions.assertEquals(athlete5.getForce(), forceAthlete5);
+        Assertions.assertEquals(athlete5.getAgilite(), agiliteAthlete5);
+        Assertions.assertEquals(athlete5.getEndurance(), endurenceAthlete5);
 
         
     }
