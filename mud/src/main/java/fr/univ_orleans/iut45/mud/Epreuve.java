73321d2a804ca19ed57353781c7c79470cc34763
Leo-sau <leo.saunier@etu.univ-orleans.fr>
2024-06-05 21:09:32 +0200

    mise à jour interface Epreuve

--- mud/src/main/java/fr/univ_orleans/iut45/mud/Epreuve.java
+++ mud/src/main/java/fr/univ_orleans/iut45/mud/Epreuve.java
@@ -1,10 +1,11 @@
 package fr.univ_orleans.iut45.mud;
 
-public interface Epreuve {
+public interface Epreuve<Athlete> {
 
     public String classementEpreuve();
-    public int getScore(Object o);
-    public void setScore(Object o, Integer valeur);
+    public int getScore(Athlete o);
+    public void setScore(Athlete o, Integer valeur);
+    public int getScoreTheorique
     public String getNom();
 
 }
