aee8b9b24b971a974ce7151511eebf58db8877a3
pierre <gangneuxpierre@gmail.com>
2024-06-10 18:24:56 +0200

    executable JO

--- mud/src/main/java/fr/univ_orleans/iut45/mud/Pays.java
+++ mud/src/main/java/fr/univ_orleans/iut45/mud/Pays.java
@@ -1,15 +1,23 @@
 package fr.univ_orleans.iut45.mud;
 
+import java.util.Set;
+
 public class Pays {
     private String nom;
+    private int compteurMedaille;
+    private int compteurMedailleOr;
 
     public Pays(String nom){
         this.nom = nom;
+        this.compteurMedaille = 0;
+        this.compteurMedailleOr = 0;
     }
 
     public String getNom() {
         return this.nom;
     }
+
+    
 
     @Override
     public boolean equals(Object objet){
@@ -25,4 +33,6 @@ public class Pays {
         return this.getNom().hashCode();
         
     }
+
+    public static Set<Pays> ()
 }
