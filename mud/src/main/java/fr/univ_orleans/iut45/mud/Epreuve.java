4791328b681adb4792d86d2b1d085d4eae4bebcc
Leo-sau <leo.saunier@etu.univ-orleans.fr>
2024-06-10 20:42:06 +0200

    Maj interface Epreuve +ajout du classementTheorique

--- mud/src/main/java/fr/univ_orleans/iut45/mud/Epreuve.java
+++ mud/src/main/java/fr/univ_orleans/iut45/mud/Epreuve.java
@@ -6,8 +6,9 @@ public interface Epreuve<T> {
     public String classementEpreuve();
     public int getScore(T o);
     public void setScore(T o, Integer valeur);
-    public int getScoreTheorique();
+    public int getScoreTheorique(T o);
     public String getNom();
+    public String classementTheorique();
 
 }
 
