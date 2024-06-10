75594c67c94028d21819970bd26be31ba79d57cf
Leo-sau <leo.saunier@etu.univ-orleans.fr>
2024-06-10 20:24:11 +0200

    Maj interface Epreuve

---
+++ mud/src/main/java/fr/univ_orleans/iut45/mud/EpreuveIndFem.java
@@ -0,0 +1,14 @@
+package fr.univ_orleans.iut45.mud;
+
+public class EpreuveIndFem extends EpreuveInd {
+
+    public EpreuveMasc(String nom, CompetInd competition){
+        super(nom, competition);
+        for(Participant a : competition.getParticipant()){
+            if(a.getSexe()=="F"){
+                this.scores.put(a, this.getScoreTheorique(a));
+            }
+        }
+    }
+
+}
---
+++ mud/src/main/java/fr/univ_orleans/iut45/mud/EpreuveIndMasc.java
@@ -0,0 +1,14 @@
+package fr.univ_orleans.iut45.mud;
+
+public class EpreuveIndMasc extends EpreuveInd {
+    
+    
+    public EpreuveMasc(String nom, CompetInd competition){
+        super(nom, competition);
+        for(Participant a : competition.getParticipant()){
+            if(a.getSexe()=="H"){
+                this.scores.put(a, this.getScoreTheorique(a));
+            }
+        }
+    }
+}
