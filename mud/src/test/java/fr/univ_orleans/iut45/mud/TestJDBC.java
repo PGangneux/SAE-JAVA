689d5a935cf1dd3d063c0988128546c491f618e6
Nathan Randriantsoa <134612654+Kyxtaka@users.noreply.github.com>
2024-06-18 17:50:10 +0200

    Test jdbc (#95)
    * ajout debut TestJDBC
    
    * ajout test isConnect

--- mud/src/test/java/fr/univ_orleans/iut45/mud/TestJDBC.java
+++ mud/src/test/java/fr/univ_orleans/iut45/mud/TestJDBC.java
@@ -8,7 +8,10 @@ import java.util.Set;
 
 import javax.print.attribute.standard.RequestingUserName;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 import fr.univ_orleans.iut45.mud.items.Athlete;
 import fr.univ_orleans.iut45.mud.items.ImportData;
 import fr.univ_orleans.iut45.mud.items.Pays;
@@ -27,7 +30,7 @@ public class TestJDBC {
     @BeforeAll
     public static void setUp() throws ClassNotFoundException, SQLException {
         chemin = "./src/test/java/fr/univ_orleans/iut45/mud/data/donnees.csv";
-        String server = new String("192.168.202.168");
+        String server = new String("192.168.202.208");
         String baseName = new String("SAE");
         String user = new String("admin");
         String password = new String("admin");
@@ -36,6 +39,10 @@ public class TestJDBC {
         liAthletes = donnees.getListAthletes();
         laConnexion = new Connexion();
         laConnexion.connecter(server, baseName, user, password);
+    }
 
+    @Test
+    public void testConnection() {
+        Assertions.assertEquals(true, laConnexion.isConnecte());
     }
 }
