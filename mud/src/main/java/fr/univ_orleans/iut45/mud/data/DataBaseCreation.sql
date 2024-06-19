4a672d387a97fc49e82014303eb54b1104a53d83
Nathan Randriantsoa <randriantsoanathan1@gmail.com>
2024-06-19 10:48:44 +0200

    ajout DB compte utilisateur

--- mud/src/main/java/fr/univ_orleans/iut45/mud/data/DataBaseCreation.sql
+++ mud/src/main/java/fr/univ_orleans/iut45/mud/data/DataBaseCreation.sql
@@ -24,11 +24,6 @@ create table ATHLETE(
     idPays int
 );
 
-create table EQUIPE (
-    idEquipe int primary key,
-    nomEquipe varchar(50)
-);
-
 create table COMPETITION (
     idCompet int primary key,
     idSport int references SPORT.idSport,
@@ -36,6 +31,14 @@ create table COMPETITION (
     individuelle int not null
     constraint chk_ind CHECK (individuelle in (0,1))
 );
+
+create table EQUIPE (
+    idEquipe int primary key,
+    nomEquipe varchar(50),
+    idPays int references PAYS.idPays,
+    idSport references SPORT.idSport
+);
+
 
 create table EPREUVE(
     idEpreuve int primary key,
@@ -71,11 +74,33 @@ create role 'organisateur';
 flush privileges;
 
 -- attibution des rôles
+grant usage on *.* to 'administrateur';
 grant all privileges on SAE.* to 'administrateur' with grant option;
+flush privileges;
+
+grant usage on *.* to 'journalist';
+grant all privileges on *.* to 'journalist';
+flush privileges;
+
+grant usage on *.* to 'organisateur';
 grant select,update,insert on SAE.* to 'organisateur';
-grant select on SAE.* to 'journalist';
 flush privileges;
 
 -- creation d'un super user
 create user 'admin'@'%' identified by 'admin';
+create user 'admin'@'localhost' identified by 'admin';
 grant 'administrateur' to 'admin'@'%';
+grant 'administrateur' to 'admin'@'localhost';
+flush privileges;
+
+create user 'journalist'@'%' identified by 'admin';
+create user 'journalist'@'localhost' identified by 'admin';
+grant 'journalist' to 'journalist'@'%';
+grant 'journalist' to 'journalist'@'localhost';
+flush privileges;
+
+create user 'organisateur'@'%' identified by 'admin';
+create user 'organisateur'@'localhost' identified by 'admin';
+grant 'organisateur' to 'organisateur'@'%';
+grant 'organisateur' to 'organisateur'@'localhost';
+flush privileges;
