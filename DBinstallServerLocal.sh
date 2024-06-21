e796c9c686416ea41f566fb3f23181838d971768
Nathan Randriantsoa <randriantsoanathan1@gmail.com>
2024-06-21 12:07:01 +0200

    edit

--- DBinstallServerLocal.sh
+++
@@ -1,8 +0,0 @@
-set +e
-set -x
-mysql -u nathan -p'ol' -h'192.168.62.208'<<EOFMYSQL 
-source ./src/main/java/fr/univ_orleans/iut45/mud/data/DataBaseCreation.sql
-source ./src/main/java/fr/univ_orleans/iut45/mud/data/UserDataCreation.sql
-source ./src/main/java/fr/univ_orleans/iut45/mud/data/apploginInit.sql
-show databases;
-
